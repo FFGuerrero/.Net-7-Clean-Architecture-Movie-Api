405a999cf7293100cb32af66c37de5c6aa39b810
Freddy Guerrero <ffguerrero01@gmail.com>
2022-12-28 21:35:45 -0400

    Fix files format

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -8,5 +8,7 @@ public interface IApplicationDbContext
 
     DbSet<TodoItem> TodoItems { get; }
 
+    DbSet<Movie> Movies { get; }
+
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }

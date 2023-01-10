7381830d19a706722797967a6fd6da43cb563241
Freddy Guerrero <ffguerrero01@gmail.com>
2023-01-09 22:24:04 -0400

    Add new columns to Application User table

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -3,4 +3,7 @@
 namespace MovieApi.Infrastructure.Identity;
 public class ApplicationUser : IdentityUser
 {
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
 }

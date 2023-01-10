2fd8da90e632f9b76caeddd47d01a35d7784b342
Freddy Guerrero <ffguerrero01@gmail.com>
2023-01-09 22:44:38 -0400

    Rename identity tables

--- src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Reflection.Emit;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -35,6 +36,41 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         base.OnModelCreating(builder);
+
+        builder.Entity<IdentityUser>(b =>
+        {
+            b.ToTable("Users");
+        });
+
+        builder.Entity<IdentityUserClaim<string>>(b =>
+        {
+            b.ToTable("UserClaims");
+        });
+
+        builder.Entity<IdentityUserLogin<string>>(b =>
+        {
+            b.ToTable("UserLogins");
+        });
+
+        builder.Entity<IdentityUserToken<string>>(b =>
+        {
+            b.ToTable("UserTokens");
+        });
+
+        builder.Entity<IdentityRole>(b =>
+        {
+            b.ToTable("Roles");
+        });
+
+        builder.Entity<IdentityRoleClaim<string>>(b =>
+        {
+            b.ToTable("RoleClaims");
+        });
+
+        builder.Entity<IdentityUserRole<string>>(b =>
+        {
+            b.ToTable("UserRoles");
+        });
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)

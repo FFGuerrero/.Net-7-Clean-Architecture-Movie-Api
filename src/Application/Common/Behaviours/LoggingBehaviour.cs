b69d589e336c18205c6425d55a667f84dc027cd1
Freddy Guerrero <ffguerrero01@gmail.com>
2023-01-09 22:08:57 -0400

    Fix unit test project compilation error

--- src/Application/Common/Behaviours/LoggingBehaviour.cs
+++ src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -7,11 +7,13 @@ public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where T
 {
     private readonly ILogger _logger;
     private readonly ICurrentUserService _currentUserService;
+    private readonly IIdentityService _identityService;
 
-    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService, IIdentityService identityService)
     {
         _logger = logger;
         _currentUserService = currentUserService;
+        _identityService = identityService;
     }
 
     public async Task Process(TRequest request, CancellationToken cancellationToken)
@@ -21,6 +23,6 @@ public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where T
         string userName = _currentUserService.UserName ?? string.Empty;
 
         await Task.Run(() => _logger.LogInformation("MovieApi Request: {Name} {@UserId} {@UserName} {@Request}",
-            requestName, userId, userName, request), cancellationToken);
+            requestName, userId, userName, request));
     }
 }

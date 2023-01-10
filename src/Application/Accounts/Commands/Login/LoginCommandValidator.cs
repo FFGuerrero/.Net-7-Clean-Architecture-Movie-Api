04d3a277f003dfecd8d1726acc02348f0645ed7c
Freddy Guerrero <ffguerrero01@gmail.com>
2023-01-09 22:17:47 -0400

    Add login command validator

---
+++ src/Application/Accounts/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace MovieApi.Application.Accounts.Commands.Login;
+public class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(v => v.UserName).NotEmpty().MaximumLength(256);
+        RuleFor(v => v.Password).NotEmpty().MaximumLength(256);
+    }
+}

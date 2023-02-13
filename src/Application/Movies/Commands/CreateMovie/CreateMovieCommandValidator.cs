3e4222941179c5f3a987f238bd301447df093cb5
Freddy Guerrero <ffguerrero01@gmail.com>
2023-02-12 23:11:38 -0400

    Add movie image validator

--- src/Application/Movies/Commands/CreateMovie/CreateMovieCommandValidator.cs
+++ src/Application/Movies/Commands/CreateMovie/CreateMovieCommandValidator.cs
@@ -17,10 +17,35 @@ public class CreateMovieCommandValidator : AbstractValidator<CreateMovieCommand>
         RuleFor(v => v.Stock).GreaterThanOrEqualTo(0);
         RuleFor(v => v.RentalPrice).GreaterThanOrEqualTo(0);
         RuleFor(v => v.SalePrice).GreaterThanOrEqualTo(0);
+        RuleFor(v => v.MovieImages).Must(BeValidUri)
+            .WithMessage("You must enter at least one valid image URL");
     }
 
     public async Task<bool> BeUniqueTitle(string title, CancellationToken cancellationToken)
     {
         return await _movieService.IsUniqueTitle(title, cancellationToken);
+    }
+
+    public static bool BeValidUri(List<string>? userImages)
+    {
+        if (userImages is null)
+        {
+            return false;
+        }
+
+        if (userImages.Count == 0)
+        {
+            return false;
+        }
+
+        foreach (var uri in userImages)
+        {
+            if (!Uri.TryCreate(uri, UriKind.Absolute, out _))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
